fe225b6ba61980f04f3797e0143cb83611f07b54
HITEN JAIN <hitenjain88@gmail.com>
2019-09-05 03:35:03 +0530

    added video recording using ss

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-import video
+from video import Video
+from video import CreateVideo
 import os
+import time
 
 class Sync():
-	url = ""
+    url = ""
+
 
 if __name__ == '__main__':
-	path = os.getcwd()+"\\flask\\uploads\\"
-	file = os.listdir(path)[0]
-	path+=file
-	v = video.video("test", path)
-	v.run()
+    path = os.getcwd() + "\\flask\\uploads\\"
+    file = os.listdir(path)[0]
+    name = str(round(time.time() * 1000)) + ".mp4"
+    path+=name
 
-	
+    createVideo = CreateVideo(path)
+
+    
